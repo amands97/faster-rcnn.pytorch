d656f81a085d82316f9e7189031c4624823a2b15
amands <aman150084@gmail.com>
2020-05-22 01:18:01 +0530

    check size

--- lib/model/faster_rcnn/faster_rcnn.py
+++ lib/model/faster_rcnn/faster_rcnn.py
@@ -81,7 +81,7 @@ class _fasterRCNN(nn.Module):
             pooled_feat = self.RCNN_roi_align(base_feat, rois.view(-1, 5))
         elif cfg.POOLING_MODE == 'pool':
             pooled_feat = self.RCNN_roi_pool(base_feat, rois.view(-1,5))
-
+        print(pooled_feat.size())
         # feed pooled features to top model
         pooled_feat = self._head_to_tail(pooled_feat)
 
